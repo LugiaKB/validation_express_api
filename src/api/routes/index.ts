e7fb279bed58275fa3c516a6c6835d9875c18926
Iury Mikael <mikaeliury@gmail.com>
2022-07-26 03:15:34 -0300

    CRUD and validation complete for Orders table

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -3,7 +3,8 @@ import express, {Express, Router} from 'express';
 import office from './OfficeRoute';
 import employee from './EmployeeRoute';
 import productLine from './ProductLineRoute';
-import customer from './CustomerRoute'
+import customer from './CustomerRoute';
+import order from './OrderRoute';
 
 const routes: Router = express.Router();
 
@@ -11,5 +12,6 @@ routes.use('/office', office);
 routes.use('/employee', employee);
 routes.use('/productline', productLine);
 routes.use('/customer', customer);
+routes.use('/order', order);
 
 export default routes;
